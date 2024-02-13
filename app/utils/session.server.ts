dde0ca37b7d614bc90e001cc0b47616fde94642e
Semion Krapivin <homer1996@gmail.com>
2024-02-13 13:47:15 +0100

    fix: create themeAction route file

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,16 +1,16 @@
 import {createCookieSessionStorage} from "@remix-run/node";
 import {createThemeSessionResolver} from "remix-themes";
 
-const sessonStorage = createCookieSessionStorage({
+const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__remix-themes",
     domain: process.env.NODE_ENV !== "development" ? "fooURL" : undefined,
     path: "/",
     httpOnly: true,
     sameSite: "lax",
-    secrets: ["secret"],
+    secrets: ["s3cr3t"],
     secure: process.env.NODE_ENV === "development" ? false : true,
   },
 });
 
-export const themeSessionResolver = createThemeSessionResolver(sessonStorage);
+export const themeSessionResolver = createThemeSessionResolver(sessionStorage);
