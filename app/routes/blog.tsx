99833fd64aded8a8483f68f72b295584f63b0494
Semion Krapivin <homer1996@gmail.com>
2024-03-26 16:41:08 +0100

    feat: add blog template

---
+++ app/routes/blog.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Blog() {
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-2 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:leading-14">
+            All Blog Posts
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Blog;
