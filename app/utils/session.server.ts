c8ffcfac2563728f88296fac26090d5f7213be12
Semion Krapivin <homer1996@gmail.com>
2024-04-01 12:54:08 +0200

    feat: change session.server

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -4,12 +4,11 @@ import {createThemeSessionResolver} from "remix-themes";
 const sessionStorage = createCookieSessionStorage({
   cookie: {
     name: "__remix-themes",
-    domain: process.env.NODE_ENV !== "development" ? "fooURL" : undefined,
     path: "/",
     httpOnly: true,
     sameSite: "lax",
     secrets: ["s3cr3t"],
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    secure: process.env.NODE_ENV === "production",
   },
 });
 
