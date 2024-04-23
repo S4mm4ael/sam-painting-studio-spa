35cd200332083a864d311c4c87339db9fd2cd437
Siamion Krapivin <homer1996@gmail.com>
2024-04-23 17:21:06 +0200

    refactor: move app component aside ofroot

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,9 +1,3 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
 import {PassThrough} from "node:stream";
 
 import type {AppLoadContext, EntryContext} from "@remix-run/node";
@@ -18,11 +12,7 @@ export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  // This is ignored so we can keep it in the template for visibility.  Feel
-  // free to delete this parameter in your app if you're not using it!
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  loadContext: AppLoadContext
+  remixContext: EntryContext
 ) {
   return isbot(request.headers.get("user-agent") || "")
     ? handleBotRequest(request, responseStatusCode, responseHeaders, remixContext)
--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,9 @@
 import type {LinksFunction, LoaderFunctionArgs} from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from "@remix-run/react";
-import {PreventFlashOnWrongTheme, ThemeProvider, useTheme} from "remix-themes";
+import {useLoaderData} from "@remix-run/react";
+import {ThemeProvider} from "remix-themes";
 import stylesheet from "~/tailwind.css";
 import {themeSessionResolver} from "./utils";
-import {ReactNode} from "react";
-import {Layout} from "./components/Layout";
+import App from "./app";
 
 export const links: LinksFunction = () => [{rel: "stylesheet", href: stylesheet}];
 
@@ -29,29 +20,5 @@ export default function AppWithProvider() {
     <ThemeProvider specifiedTheme={theme} themeAction="/action/set-theme">
       <App />
     </ThemeProvider>
-  );
-}
-
-function App() {
-  const {theme} = useLoaderData<typeof loader>();
-  const [themeX] = useTheme();
-  return (
-    <html lang="en" data-theme={themeX ?? ""}>
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <PreventFlashOnWrongTheme ssrTheme={Boolean(theme)} />
-        <Links />
-      </head>
-      <body className="bg-gradient-to-r from-white dark:from-blue-900 from-0% via-slate-200 dark:via-blue-950 to-slate-400 dark:to-black text-black dark:text-white dark:bg-gray-900 h-full selection:bg-gray-50 dark:selection:bg-gray-800">
-        <Layout>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </Layout>
-      </body>
-    </html>
   );
 }
