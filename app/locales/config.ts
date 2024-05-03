44ceed8db061694ce1154b2bc610ed030e6b2615
Siamion Krapivin <homer1996@gmail.com>
2024-05-03 16:40:41 +0200

    feat: add to i18 config

--- app/locales/config.ts
+++ app/locales/config.ts
@@ -1,23 +1,23 @@
+/* eslint-disable import/no-named-as-default-member */
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import {initReactI18next} from 'react-i18next';
+import textEn from './translation/en.json';
+
+console.log(JSON.stringify(textEn));
 
 i18next
   .use(LanguageDetector)
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     debug: true,
     fallbackLng: 'en',
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false,
     },
     resources: {
       en: {
-        translation: {
-          // here we will place our translations...
-        },
+        translation: JSON.stringify(textEn),
       },
     },
   });
